f2df519bbe3351fcd809eb22ee605e71e83e3980
gaozhifan <gaozhifan@eseasky.cn>
2019-08-20 17:05:09 +0800

    pls

--- framework/AuthService/src/main/java/com/eseasky/core/framework/AuthService/controller/UserController.java
+++ framework/AuthService/src/main/java/com/eseasky/core/framework/AuthService/controller/UserController.java
@@ -6,14 +6,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import io.swagger.annotations.Api;
-import lombok.extern.log4j.Log4j2;
 
 /**
 *  用户信息控制器
 */
 @Api(value = "用户管理",tags = "用户管理服务")
 @RestController
-@Log4j2
 public class UserController {
 
     /**
