d8ceab28e0269904031fc01942f4741d5413e532
gaozhifan <gaozhifan@eseasky.cn>
2019-08-20 11:46:06 +0800

    去掉token客户端

--- framework/AuthService/src/main/java/com/eseasky/core/framework/AuthService/controller/UserController.java
+++ framework/AuthService/src/main/java/com/eseasky/core/framework/AuthService/controller/UserController.java
@@ -1,31 +1,11 @@
 package com.eseasky.core.framework.AuthService.controller;
 
 import java.security.Principal;
-import java.util.List;
-import java.util.stream.Collectors;
 
-import com.eseasky.core.framework.AuthService.module.model.AuthAccessToken;
-import com.eseasky.core.framework.AuthService.module.repository.AuthAccessTokenRepository;
-import org.springframework.beans.BeanUtils;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.alibaba.fastjson.JSONObject;
-import com.eseasky.core.framework.AuthService.module.model.ServUserInfo;
-import com.eseasky.core.framework.AuthService.module.service.ServUserInfoService;
-import com.eseasky.core.framework.AuthService.protocol.dto.ServUserInfoDTO;
-import com.eseasky.core.framework.AuthService.protocol.vo.ServUserInfoVO;
-import com.eseasky.global.entity.MsgPageInfo;
-import com.eseasky.global.entity.ResultModel;
-
 import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
 import lombok.extern.log4j.Log4j2;
 
 /**
