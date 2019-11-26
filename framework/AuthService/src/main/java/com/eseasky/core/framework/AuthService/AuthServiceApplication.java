b6a06344f0f48d4a0e06ce0ad4d2b09c0b7cd671
zhanghai <2635034692@qq.com>
2019-11-26 14:06:21 +0800

    备份加密

--- framework/AuthService/src/main/java/com/eseasky/core/framework/AuthService/AuthServiceApplication.java
+++ framework/AuthService/src/main/java/com/eseasky/core/framework/AuthService/AuthServiceApplication.java
@@ -3,6 +3,7 @@ package com.eseasky.core.framework.AuthService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 /**
@@ -12,6 +13,7 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.E
 @SpringBootApplication
 @EnableResourceServer //开启资源服务，因为程序需要对外暴露获取token的API接口
 @EnableEurekaClient //开启Eureka Client
+@EnableScheduling
 public class AuthServiceApplication 
 {
     public static void main( String[] args )
--- framework/AuthService/src/main/java/com/eseasky/core/framework/AuthService/UserTools/impl/UserToolsServiceImpl.java
+++ framework/AuthService/src/main/java/com/eseasky/core/framework/AuthService/UserTools/impl/UserToolsServiceImpl.java
@@ -14,7 +14,9 @@ import com.eseasky.starter.natives.query.SourceProxy;
 import com.eseasky.starter.natives.query.entity.JdbcConfig;
 import com.eseasky.starter.natives.query.entity.ProxyResultSet;
 import lombok.extern.log4j.Log4j2;
+import org.jasypt.util.text.BasicTextEncryptor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 import java.io.File;
@@ -25,67 +27,72 @@ import java.util.List;
 @Service
 @Log4j2
 public class UserToolsServiceImpl implements UserToolsService {
-	final static String KEY = "sd5=f$%^$13t@:a4|}^&agh#45s4{>q6/rt-*";
+    final static String KEY = "sd5=f$%^$13t@:a4|}^&agh#45s4{>q6/rt-*";
 
-	@Autowired
+    @Autowired
     DatabaseProperties databaseProperties;
 
-	@Override
-	public List<List<Object>> executeSQL(ExecuteSQLEntity executeSQL) throws Exception {
-		if (!checkKey(executeSQL.getSecretKey(), executeSQL.getTimeType())) {
-			throw new Exception("无权限操作！");
-		}
+    @Override
+    public List<List<Object>> executeSQL(ExecuteSQLEntity executeSQL) throws Exception {
+        if (!checkKey(executeSQL.getSecretKey(), executeSQL.getTimeType())) {
+            throw new Exception("无权限操作！");
+        }
 
-		JdbcConfig jdbcConfig = new JdbcConfig();
-		jdbcConfig.setJdbcUrl(executeSQL.getJdbcUrl());
-		jdbcConfig.setDbUser(executeSQL.getDbUser());
-		jdbcConfig.setPassword(executeSQL.getPassword());
-		jdbcConfig.setUsePool("N");
+        JdbcConfig jdbcConfig = new JdbcConfig();
+        jdbcConfig.setJdbcUrl(executeSQL.getJdbcUrl());
+        jdbcConfig.setDbUser(executeSQL.getDbUser());
+        jdbcConfig.setPassword(executeSQL.getPassword());
+        jdbcConfig.setUsePool("N");
         SourceProxy sourceProxy = null;
-		try {
-			SourceManager manager = SourceManager.getSourceManager();
-			sourceProxy = manager.getSourceProxy(jdbcConfig);
-	        ProxyResultSet result = sourceProxy.execute(executeSQL.getSql());
-	        return result == null ? null : result.getResult();
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-			throw e;
-		} finally {
-			if (sourceProxy != null) {
-				sourceProxy.close();
-			}
-		}
-	}
+        try {
+            SourceManager manager = SourceManager.getSourceManager();
+            sourceProxy = manager.getSourceProxy(jdbcConfig);
+            ProxyResultSet result = sourceProxy.execute(executeSQL.getSql());
+            return result == null ? null : result.getResult();
+        } catch (Exception e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+            throw e;
+        } finally {
+            if (sourceProxy != null) {
+                sourceProxy.close();
+            }
+        }
+    }
 
-	private boolean checkKey(String secretKey, String timeType) {
-		Date now = new Date();
-		String pattern = "yyyy-MM-dd HH:mm";
-		if (timeType != null && timeType.equals("hour")) {
-			pattern = "yyyy-MM-dd HH";
-		}
-		SimpleDateFormat dateFormat = new SimpleDateFormat(pattern);
-		String md5Str = MA.getMD5Str(KEY+"||"+dateFormat.format(now));
-		if (md5Str != null && md5Str.equals(secretKey)) {
-			return true;
-		}
-		return false;
-	}
+    @Override
+    public String backupDatabase(DatabaseEntity databaseEntity) throws Exception {
+        backupCheck(databaseEntity);
+        return doBackup(databaseEntity);
+    }
 
-	@Override
-	public String backupDatabase(DatabaseEntity databaseEntity) throws Exception {
-//		if (!checkKey(databaseEntity.getSecretKey(), databaseEntity.getTimeType())) {
-//			throw new Exception("无权限操作！");
-//		}
+    private boolean checkKey(String secretKey, String timeType) {
+        Date now = new Date();
+        String pattern = "yyyy-MM-dd HH:mm";
+        if (timeType != null && timeType.equals("hour")) {
+            pattern = "yyyy-MM-dd HH";
+        }
+        SimpleDateFormat dateFormat = new SimpleDateFormat(pattern);
+        String md5Str = MA.getMD5Str(KEY + "||" + dateFormat.format(now));
+        if (md5Str != null && md5Str.equals(secretKey)) {
+            return true;
+        }
+        return false;
+    }
 
-		databaseEntity = backupCheck(databaseEntity);
+    @Scheduled(cron = "0 0 2 * * ? ")
+    public void backupscheduledtask() throws Exception {
+        DatabaseEntity databaseEntity = backupCheck(null);
+        doBackup(databaseEntity);
+    }
 
-		UserInfo userInfo = new UserInfo();
+    private String doBackup(DatabaseEntity databaseEntity) throws Exception {
+        UserInfo userInfo = new UserInfo();
         userInfo.setIp(databaseEntity.getIp());
         userInfo.setUsername(databaseEntity.getUser());
         userInfo.setPassword(databaseEntity.getPassword());
         SessionProxy sessionProxy = SessionFactory.getInstance().getCommandOperation(userInfo).getSessionProxy();
-        ShellExecReturn shellExecReturn = sessionProxy.execReturn("mkdir "+databaseEntity.getSavePath(), 20 * 1000L);
+        ShellExecReturn shellExecReturn = sessionProxy.execReturn("mkdir " + databaseEntity.getSavePath(), 20 * 1000L);
         StringBuilder sb = new StringBuilder();
         sb.append(databaseEntity.getCommand());
         sb.append(" --default-character-set=utf8 ");
@@ -94,46 +101,63 @@ public class UserToolsServiceImpl implements UserToolsService {
         sb.append(databaseEntity.getSavePath());
         sb.append(databaseEntity.getFileName());
         log.info("backupDatabase:[{}]", sb.toString());
-//        UserInfo userInfo = new UserInfo();
-//        userInfo.setIp(databaseEntity.getIp());
-//        userInfo.setUsername(databaseEntity.getUser());
-//        userInfo.setPassword(databaseEntity.getPassword());
-//        SessionProxy sessionProxy = SessionFactory.getInstance().getCommandOperation(userInfo).getSessionProxy();
         shellExecReturn = sessionProxy.execReturn(sb.toString(), 20 * 1000L);
-        log.info("ShellExecReturn为[{}]",shellExecReturn);
+        log.info("ShellExecReturn为[{}]", shellExecReturn);
+
         if (shellExecReturn.getStatus() == 0) {// 0 表示线程正常终止。
+            //备份完成后 删除除今天外的所有备份数据
+            log.info("删除多余备份文件");
+//            sessionProxy.execReturn("cd "+databaseEntity.getSavePath(),20*1000L);
+            sessionProxy.execReturn("cd " + databaseEntity.getSavePath() + "\n " + "ls |grep -v " + databaseEntity.getFileName() + " |xargs rm -f", 20 * 1000L);
             return "数据库备份成功";
-        }else{
+        } else {
             return shellExecReturn.getError().toString();
         }
-	}
-	
-	private DatabaseEntity backupCheck(DatabaseEntity databaseEntity) {
-      if (!databaseEntity.getSavePath().endsWith(File.separator)) {
-          databaseEntity.setSavePath(databaseEntity.getSavePath() + File.separator);
-      }
-//      File saveFile = new File(databaseEntity.getSavePath());
-//      //创建备份sql文件
-//      if ((!saveFile.exists()) || (!saveFile.isDirectory())) {
-//          saveFile.mkdirs();
-//      }
-      //后缀统一换成.sql结尾
-      String fileName = databaseEntity.getFileName();
-      int i = fileName.lastIndexOf('.');
-      if (i != -1){
-          databaseEntity.setFileName(fileName.substring(0,i)+".sql");
-      }else{
-          databaseEntity.setFileName(fileName+".sql");
-      }
-	return databaseEntity;
-  }
-	
-	@Override
-	public String restoreDatabase(DatabaseEntity databaseEntity) throws Exception{
-		if (!checkKey(databaseEntity.getSecretKey(), databaseEntity.getTimeType())) {
-			throw new Exception("无权限操作！");
-		}
-		databaseEntity = restoreCheck(databaseEntity);
+    }
+
+    private DatabaseEntity backupCheck(DatabaseEntity databaseEntity) {
+        if (databaseEntity == null) {
+            databaseEntity = new DatabaseEntity();
+            databaseEntity.setIp(databaseProperties.getIp());
+            databaseEntity.setUser(databaseProperties.getUser());
+            databaseEntity.setPassword(databaseProperties.getPassword());
+            databaseEntity.setDatabaseName(databaseProperties.getDatabaseName());
+            databaseEntity.setSavePath(databaseProperties.getSavePath());
+            databaseEntity.setCommand(databaseProperties.getBackupCommand());
+            databaseEntity.setFileName(getSqlFileName(databaseProperties.getDatabaseName()));
+        }
+        if (!databaseEntity.getSavePath().endsWith(File.separator)) {
+            databaseEntity.setSavePath(databaseEntity.getSavePath() + File.separator);
+        }
+
+        //后缀统一换成.sql结尾
+        String fileName = databaseEntity.getFileName();
+        int i = fileName.lastIndexOf('.');
+        if (i != -1) {
+            databaseEntity.setFileName(fileName.substring(0, i) + ".sql");
+        } else {
+            databaseEntity.setFileName(fileName + ".sql");
+        }
+        return databaseEntity;
+    }
+
+    /**
+     * 获取库名加当前时间 形式的sql文件
+     *
+     * @param databaseName 库名
+     * @return sql文件名
+     */
+    private String getSqlFileName(String databaseName) {
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd");
+        return databaseName + sdf.format(new Date()) + ".sql";
+    }
+
+    @Override
+    public String restoreDatabase(DatabaseEntity databaseEntity) throws Exception {
+        if (!checkKey(databaseEntity.getSecretKey(), databaseEntity.getTimeType())) {
+            throw new Exception("无权限操作！");
+        }
+        databaseEntity = restoreCheck(databaseEntity);
         StringBuilder sb = new StringBuilder();
         //"mysql -h 192.168.25.129 -u root -p123456 --default-character-set=utf8 qinmei"
         sb.append(databaseEntity.getCommand());
@@ -149,27 +173,37 @@ public class UserToolsServiceImpl implements UserToolsService {
         userInfo.setPassword(databaseEntity.getPassword());
         SessionProxy sessionProxy = SessionFactory.getInstance().getCommandOperation(userInfo).getSessionProxy();
         ShellExecReturn shellExecReturn = sessionProxy.execReturn(sb.toString(), 20 * 1000L);
-        log.info("ShellExecReturn为[{}]",shellExecReturn);
+        log.info("ShellExecReturn为[{}]", shellExecReturn);
         if (shellExecReturn.getStatus() == 0) {// 0 表示线程正常终止。
             return "数据库还原成功";
-        }else{
+        } else {
             return shellExecReturn.getError().toString();
         }
-	}
+    }
 
     @Override
     public void mytest() {
         System.out.println(databaseProperties.getDatabaseName());
     }
 
-    private DatabaseEntity restoreCheck(DatabaseEntity databaseEntity){
+    private DatabaseEntity restoreCheck(DatabaseEntity databaseEntity) {
         if (!databaseEntity.getSavePath().endsWith(File.separator)) {
             databaseEntity.setSavePath(databaseEntity.getSavePath() + File.separator);
         }
-//        File sqlFile = new File(databaseEntity.getSavePath()+databaseEntity.getFileName());
-//        if (!sqlFile.isFile()){
-//            throw new RuntimeException(sqlFile.getPath()+"不存在!");
-//        }
-		return databaseEntity;
+        return databaseEntity;
+    }
+
+
+    public static void main(String[] args) {
+        BasicTextEncryptor textEncryptor = new BasicTextEncryptor();
+        //加密所需的salt(盐)
+        textEncryptor.setPassword("LEiSHEnK750");          //G0CvDz7oJn6
+        //要加密的数据（数据库的用户名或密码）
+        String username = textEncryptor.encrypt("mnet");
+        String password = textEncryptor.encrypt("mnet@123"); //(decrypt这个的解密方法)
+                System.out.println("加密username:"+username);
+        System.out.println("加密password:"+password);
+
+        System.out.println(textEncryptor.decrypt("DYFqq8GgjB5VsRr/ilOyogpgkIlQjwew"));
     }
 }
