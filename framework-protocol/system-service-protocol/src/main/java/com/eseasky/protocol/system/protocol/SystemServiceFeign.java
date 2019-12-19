6042aab7ed070b8f58f2c6f11be1dd6ef7ef5417
clq <727783937@qq.com>
2019-12-19 19:10:03 +0800

    system协议

---
+++ framework-protocol/system-service-protocol/src/main/java/com/eseasky/protocol/system/protocol/SystemServiceFeign.java
@@ -0,0 +1,19 @@
+package com.eseasky.protocol.system.protocol;
+
+import com.eseasky.core.starters.feign.wrapper.config.Feign;
+import com.eseasky.global.entity.MsgReturn;
+import com.eseasky.protocol.system.entity.DTO.DictiCondiDTO;
+import com.eseasky.protocol.system.entity.VO.DictItemVO;
+import feign.Headers;
+import feign.RequestLine;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@Feign(serviceName="SystemService")
+public interface SystemServiceFeign {
+
+    @RequestLine("POST /dict/queryByKeyAndDictId")
+    @Headers({"Content-Type: application/json","Accept: application/json"})
+    public ResponseEntity<MsgReturn<DictItemVO>> queryByKeyAndDictId(@RequestBody DictiCondiDTO dictiCondiDTO);
+
+}
